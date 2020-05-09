6b47bfc9979bfb7cc3e037f03dc343061f9073e4
Anderson Fernandes <andersonfferreira13@hotmail.com>
2020-05-09 15:59:34 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -10,6 +10,15 @@ public class Utils {
 	public static Stage currentStage(javafx.event.ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParsetoInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 }
 
